66ac1bc3149931440414d067014f1dd62a868f78
mapleberry14 <jmmagana@gbox.adnu.edu.ph>
2025-05-08 21:35:20 +0800

    Add files via upload

---
+++ my_app_n1am/app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    obj=Member.objects.all()
+    context={
+        "obj":obj,
+    }
+    return render(request,"index.html")
